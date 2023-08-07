1b3d7077e7151ad05c0bb63fa41c583ca08fb5d0
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-08-07 15:27:19 +0500

    Update dirinspector.py
     * Improved support for configuration files
     * Improved Windows/Linux support
     * Improved support for execution arguments
     * Changed the structural aspect of the code for better readability
     * Changed the way hidden files are handled
     * Added argument for printing without color

--- dirinspector.py
+++ dirinspector.py
@@ -1,50 +1,97 @@
 import configparser
+import platform
+import argparse
 from os import listdir as ls
 from os.path import abspath, isdir
-from sys import argv
-argv.append("")
 
-conf = configparser.ConfigParser()
-conf.read(f'{__file__[:__file__.rfind("/")-len(__file__)+1]}config.ini')
+class FileType:
+    def __init__(self, typeName, colorCode, fileList) -> None:
+        self.typeName = typeName
+        self.colorCode = colorCode
+        self.fileList = fileList
 
-def check_format(file, formats) -> bool:
-    for form in formats:
-        return "."+form in file
+    def __str__(self) -> str:
+        return f"{self.typeName=}, {self.colorCode=}, {self.fileList=}"
 
-def main():
-    directory = (abspath('')+"/", ls(abspath('')+"/"))
-    files = [["Hidden", 245, []], ["Image", 226, []], ["Video", 208, []],
-             ["Audio", 124, []], ["Document", 255, []], ["Archive", 81, []],
-             ["Code", 10, []], ["Directory", 27, []], ["Etc", 93, []]
-    ]
+    def addFile(self, fileName) -> None:
+        self.fileList.append(fileName)
+
+def checkFormat(file, formats) -> bool:
+    for form in formats.split(', '):
+        if file[-len(form)-1:] == '.'+form:
+            return True
+
+def sortFilesByFormat() -> None:
     for file in directory[1]:
-        if '.' == file[0]:
-            if argv[1].upper() == "-A":
-                files[0][2].append(file)
-        elif check_format(file, conf['formats']['image'].split(' , ')):
-            files[1][2].append(file)
-        elif check_format(file, conf['formats']['video'].split(' , ')):
-            files[2][2].append(file)
-        elif check_format(file, conf['formats']['audio'].split(' , ')):
-            files[3][2].append(file)
-        elif check_format(file, conf['formats']['document'].split(' , ')):
-            files[4][2].append(file)
-        elif check_format(file, conf['formats']['archive'].split(' , ')):
-            files[5][2].append(file)
-        elif check_format(file, conf['formats']['code'].split(' , ')):
-            files[6][2].append(file)
+        if '.' == file[0] and showall:
+            continue
+        if isdir(file):
+            fileTypes['Directory'].addFile(file)
+        elif checkFormat(file, configFileTypes['image']):
+            fileTypes['Image'].addFile(file)
+        elif checkFormat(file, configFileTypes['video']):
+            fileTypes['Video'].addFile(file)
+        elif checkFormat(file, configFileTypes['audio']):
+            fileTypes['Audio'].addFile(file)
+        elif checkFormat(file, configFileTypes['document']):
+            fileTypes['Document'].addFile(file)
+        elif checkFormat(file, configFileTypes['archive']):
+            fileTypes['Archive'].addFile(file)
+        elif checkFormat(file, configFileTypes['code']):
+            fileTypes['Code'].addFile(file)
         else:
-            if isdir(file):
-                files[7][2].append(file)
-            else:
-                files[8][2].append(file)
-    print(f"\033[38;5;10mDirectory: {directory[0]}\033[0;0m")
-    for file_type in files:
-        if file_type[2]:
-            file_type[2].sort()
-            print(f"\033[38;5;{file_type[1]}m--[{file_type[0]}]--\033[0;0m")
-            for file in file_type[2]:
-                print(f"\033[38;5;{file_type[1]}m{file}\033[0;0m")
+            fileTypes['Etc'].addFile(file)
+
+def printSortedFiles() -> None:
+    print(f"Directory: {directory[0]}") if stdout else print(f"\033[38;5;10mDirectory: {directory[0]}\033[0;0m")
+    for fileType in fileTypes:
+        fileType = fileTypes[fileType]
+        if fileType.fileList:
+            fileType.fileList.sort()
+            print(f"--[{fileType.typeName}]--") if stdout else print(f"\033[38;5;{fileType.colorCode}m--[{fileType.typeName}]--\033[0;0m")
+            for file in fileType.fileList:
+                print(file) if stdout else print(f"\033[38;5;{fileType.colorCode}m{file}\033[0;0m")
+
+def main() -> None:
+    global configFileTypes, showall, stdout, fileTypes, directory
+
+    # Config reading
+    pathSymbol = '/' if platform.system() == "Linux" else '\\'
+    fullPath = __file__
+    lastPathSymbol = fullPath.rfind(pathSymbol)
+
+    conf = configparser.ConfigParser()
+    conf.read(f'{fullPath[:lastPathSymbol-len(fullPath)+1]}config.ini')
+
+    # Setting up/Reading args
+    argparser = argparse.ArgumentParser(prog='dirinspector', description='Shows files like "ls" or "dir"')
+    
+    argparser.add_argument('-a', '--showall', default=False, action='store_true', help='show all files (show hidden ones)')
+    argparser.add_argument('-s', '--stdout', default=False, action='store_true', help='print to console without color')
+    
+    args = argparser.parse_args()
+    
+    # Setting up variables and file types
+    directory = (abspath('')+pathSymbol, ls(abspath('')+pathSymbol))
+    configFileTypes = dict(conf['formats'])
+    stdout = args.stdout
+    showall = not args.showall
+
+    fileTypes = {
+        'Hidden': FileType('Hidden', 245, []),
+        'Image': FileType('Image', 226, []),
+        'Video': FileType('Video', 208, []),
+        'Audio': FileType('Audio', 124, []),
+        'Document': FileType('Document', 255, []),
+        'Archive': FileType('Archive', 81, []),
+        'Code': FileType('Code', 10, []),
+        'Directory': FileType('Directory', 27, []),
+        'Etc': FileType('Etc', 93, [])
+    }
+
+    # Doing what we wanna do
+    sortFilesByFormat()
+    printSortedFiles()
 
 if __name__ == "__main__":
     main()
